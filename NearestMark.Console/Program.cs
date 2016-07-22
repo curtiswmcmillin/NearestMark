4cfe6ed1e432fa45ac92aadc967d88e31a8a579f
Curtis McMillin <curtis.w.mcmillin@gmail.com>
2016-07-22 09:08:35 -0500

    better ux, command-line parms

--- NearestMark.Console/Program.cs
+++ NearestMark.Console/Program.cs
@@ -6,8 +6,8 @@ namespace NearestMark
 {
     class Program
     {
-        private static Coordinates coordinatesFromFile2D = null;
-        private static Coordinates coordinatesFromFile3D = null;
+        private static Coordinates testCoordinates = null;
+        private static Coordinates inputCoordinates = null;
 
         private static Coordinates loadCoordinatesFromFile(string fileName)
         {
@@ -33,26 +33,19 @@ namespace NearestMark
             Console.WriteLine("Farthest distance is: {0}", farthestCoordinate.Distance.ToString());
         }
 
-        private static Coordinates processCoordinatesFile(string coordinatesFileName, string coordinatesInputFileName)
+        private static void processInputCoordinates()
         {
-            var coordinatesFromFile = loadCoordinatesFromFile(coordinatesFileName);
-            if (coordinatesFromFile == null)
-                return null;
-
-            var inputCoordinates = loadCoordinatesFromFile(coordinatesInputFileName);
-            if (inputCoordinates == null)
-                return null;
-
             foreach (var inputCoordinate in inputCoordinates)
             {
-                var nearestCoordinate = coordinatesFromFile.GetNearestCoordinate(inputCoordinate);
-                var farthestCoordinate = coordinatesFromFile.GetFarthestCoordinate(inputCoordinate);
+                var nearestCoordinate = testCoordinates.GetNearestCoordinate(inputCoordinate);
+                var farthestCoordinate = testCoordinates.GetFarthestCoordinate(inputCoordinate);
 
                 displayResults(inputCoordinate, nearestCoordinate, farthestCoordinate);
 
                 Console.WriteLine();
             }
-            return coordinatesFromFile;
+
+            Console.WriteLine(string.Format("{0} Coordinates compared.", inputCoordinates.Count));
         }
 
         private static void processUserInput()
@@ -61,18 +54,11 @@ namespace NearestMark
             {
                 while (true)
                 {
-                    Console.WriteLine("Please enter a 2D or 3D coordinate (comma-separated numbers) and press <ENTER>.");
+                    Console.WriteLine("Please enter a coordinate and press <ENTER>.");
 
                     var userInput = Console.ReadLine();
                     Coordinate inputCoordinate = new Coordinate(userInput);
-                    if (inputCoordinate.Points.Count == 2)
-                    {
-                        displayResults(inputCoordinate, coordinatesFromFile2D.GetNearestCoordinate(inputCoordinate), coordinatesFromFile2D.GetFarthestCoordinate(inputCoordinate));
-                    }
-                    else if (inputCoordinate.Points.Count == 3)
-                    {
-                        displayResults(inputCoordinate, coordinatesFromFile3D.GetNearestCoordinate(inputCoordinate), coordinatesFromFile3D.GetFarthestCoordinate(inputCoordinate));
-                    }
+                    displayResults(inputCoordinate, testCoordinates.GetNearestCoordinate(inputCoordinate), testCoordinates.GetFarthestCoordinate(inputCoordinate));
                     Console.WriteLine();
                 }
             }
@@ -90,27 +76,53 @@ namespace NearestMark
             }
         }
 
+        /// <summary>
+        /// Takes 2 arguments, only one is required.
+        /// Parameter 1 is the test file containing points to be compared against.
+        /// Parameter 2 is OPTIONAL, contains points to iterate through, comparing against the test file.        
+        /// Example: 
+        /// NearestMark.exe 2DTest.txt 2DInput.txt
+        /// NearestMark.exe 2DTest.txt
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Near and Far!");
-            Console.WriteLine();
-            Console.WriteLine("Press a key to begin processing 2D and 3D points(in 2D.txt and 3D.txt).");
-            Console.WriteLine();
-            Console.ReadKey();
 
-            coordinatesFromFile2D = processCoordinatesFile("2D.txt", "2DTest.txt");
-
-            coordinatesFromFile3D = processCoordinatesFile("3D.txt", "3DTest.txt");
-
-            if (coordinatesFromFile2D != null && coordinatesFromFile3D != null)
-            {
-                processUserInput();
-            }
-            else
+            if (args.Length == 0)
             {
                 Console.WriteLine();
-                Console.WriteLine("Please confirm that 2D and 3D points(in 2D.txt and 3D.txt) are located in program folder.");
+                Console.WriteLine("Please supply a test file name in the first argument.");
                 Console.ReadKey();
+                return;
+            }
+
+            if (args.Length == 2)
+            {
+                var testFileName = args[0];
+                var inputFileName = args[1];
+
+                Console.WriteLine();
+
+                Console.WriteLine(string.Format("Press a key to begin comparing test coordinates found in {0} with those in {1}.", testFileName, inputFileName));
+                Console.WriteLine();
+                Console.ReadKey();
+
+                testCoordinates = loadCoordinatesFromFile(args[0]);
+                inputCoordinates = loadCoordinatesFromFile(args[1]);
+
+                processInputCoordinates();
+                                
+                Console.ReadKey();
+
+                return;
+            }
+
+            if (args.Length == 1)
+            {
+                testCoordinates = loadCoordinatesFromFile(args[0]);
+
+                processUserInput();
             }
         }
     }
