fe25046a002ab24d6834871aeea5b100e37034a5
Curtis McMillin <curtis.w.mcmillin@gmail.com>
2016-07-22 09:59:45 -0500

    better feedback

--- NearestMark.Console/Program.cs
+++ NearestMark.Console/Program.cs
@@ -19,18 +19,18 @@ namespace NearestMark
             }
             else
             {
-                Console.WriteLine("Please confirm that this file exists: " + filePath);
+                Console.WriteLine("File not found. Please confirm that this file exists: " + filePath);
                 return null;
             }
         }
 
         private static void displayResults(Coordinate inputCoordinate, Coordinate nearestCoordinate, Coordinate farthestCoordinate)
         {
-            Console.WriteLine("Input coordinate: {0}", inputCoordinate.ToString());
-            Console.WriteLine("Nearest coordinate to input coordinate: {0}", nearestCoordinate.ToString());
-            Console.WriteLine("Nearest distance is: {0}", nearestCoordinate.Distance.ToString());
-            Console.WriteLine("Farthest coordinate from input coordinate: {0}", farthestCoordinate.ToString());
-            Console.WriteLine("Farthest distance is: {0}", farthestCoordinate.Distance.ToString());
+            Console.WriteLine("Input Coordinate: {0}", inputCoordinate.ToString());
+            Console.WriteLine("Nearest Coordinate to Input Coordinate: {0}", nearestCoordinate.ToString());
+            Console.WriteLine("Nearest Distance is: {0}", nearestCoordinate.Distance.ToString());
+            Console.WriteLine("Farthest Coordinate from Input Coordinate: {0}", farthestCoordinate.ToString());
+            Console.WriteLine("Farthest Distance is: {0}", farthestCoordinate.Distance.ToString());
         }
 
         private static void processInputCoordinates()
@@ -54,7 +54,7 @@ namespace NearestMark
             {
                 while (true)
                 {
-                    Console.WriteLine("Please enter a coordinate and press <ENTER>.");
+                    Console.WriteLine("Please enter a Coordinate (n1, n2, ...) and press <ENTER>.");
 
                     var userInput = Console.ReadLine();
                     Coordinate inputCoordinate = new Coordinate(userInput);
@@ -78,8 +78,8 @@ namespace NearestMark
 
         /// <summary>
         /// Takes 2 arguments, only one is required.
-        /// Parameter 1 is the test file containing points to be compared against.
-        /// Parameter 2 is OPTIONAL, contains points to iterate through, comparing against the test file.        
+        /// Parameter 1 is the test file name, a text file containing points (n1, n2, ...) to be compared against.
+        /// Parameter 2 is OPTIONAL, contains input points to iterate through, comparing against the test file.        
         /// Example: 
         /// NearestMark.exe 2DTest.txt 2DInput.txt
         /// NearestMark.exe 2DTest.txt
@@ -92,7 +92,7 @@ namespace NearestMark
             if (args.Length == 0)
             {
                 Console.WriteLine();
-                Console.WriteLine("Please supply a test file name in the first argument.");
+                Console.WriteLine("Please supply a test file name in the first argument (example: NearestMark.exe 2DTest.txt)");
                 Console.ReadKey();
                 return;
             }
@@ -104,7 +104,7 @@ namespace NearestMark
 
                 Console.WriteLine();
 
-                Console.WriteLine(string.Format("Press a key to begin comparing test coordinates found in {0} with those in {1}.", testFileName, inputFileName));
+                Console.WriteLine(string.Format("Press a key to begin comparing test Coordinates found in {0} with those in {1}.", testFileName, inputFileName));
                 Console.WriteLine();
                 Console.ReadKey();
 
